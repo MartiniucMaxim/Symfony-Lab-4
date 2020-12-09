b929b3bad0ae39bec096add2cf143c7603db305f
Martiniuc Maxim <martiniuc.maxim1999@gmail.com>
2020-12-09 13:42:18 +0200

    added form project

---
+++ src/Entity/Task.php
@@ -0,0 +1,27 @@
+<?php
+
+class Task
+{
+    protected $task;
+    protected $dueDate;
+
+    public function getTask(): string
+    {
+        return $this->task;
+    }
+
+    public function setTask(string $task): void
+    {
+        $this->task = $task;
+    }
+
+    public function getDueDate(): ?\DateTime
+    {
+        return $this->dueDate;
+    }
+
+    public function setDueDate(?\DateTime $dueDate): void
+    {
+        $this->dueDate = $dueDate;
+    }
+}
